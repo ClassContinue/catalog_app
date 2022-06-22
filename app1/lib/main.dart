bdf580bee77e41d68bc6d5469017fc9de0e26cdd
ClassContinue <classcontinue1@gmail.com>
2022-06-22 22:41:32 +0530

    Day 2

--- app1/lib/main.dart
+++ app1/lib/main.dart
@@ -5,13 +5,18 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({ Key? key }) : super(key: key);
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    int id = 88;
+    String name = "Bhavik Limbani";
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(),
+        body: Center(
+          child: Text("Your Id Is : $id & Name Is : $name"),
+        ),
       ),
     );
   }
